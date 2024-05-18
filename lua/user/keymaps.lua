8ca590fe8ba582dba65feb93ecf9b575df7f6862
scimania <gauravdeepubhi@gmail.com>
2024-05-18 11:42:18 +0530

    initial commit on new config

---
+++ lua/user/keymaps.lua
@@ -0,0 +1,74 @@
+local map = vim.keymap.set
+
+-- some defaults
+map("n", "Q", "<nop>")
+map("n", "<space>", "<nop>")
+
+-- keep the cursor at its position when joining lines
+map("n", "J", "mzJ`z")
+
+-- always center the screen when moving by half screen
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
+
+-- for better navigation when searching
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
+
+-- window navigation
+map("n", "<C-h>", "<C-W>h")
+map("n", "<C-j>", "<C-W>j")
+map("n", "<C-k>", "<C-W>k")
+map("n", "<C-l>", "<C-W>l")
+
+-- window resizing
+map("n", "<M-,>", "<C-W>5<")
+map("n", "<M-,>", "<C-W>5>")
+map("n", "<M-t>", "<C-W>+")
+map("n", "<M-s>", "<C-W>-")
+
+-- moving lines in visual mode
+map("v", "<M-j>", ":m '>+1<cr>gv=gv")
+map("v", "<M-k>", ":m '<-2<cr>gv=gv")
+
+-- easier indentation
+map("v", "<", "<gv")
+map("v", ">", ">gv")
+map("x", "<leader>p", '"_dP')
+
+-- for dealing with wrapping
+map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- stop hlsearch
+map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>")
+
+-- terminal escape
+map("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit Terminal Mode" })
+
+-- tab bindings
+map("n", "<leader>tt", "<cmd>tabnew|term<CR>", { desc = "Open terminal in a new tab" })
+map("n", "<leader>ts", function()
+  vim.cmd.new()
+  vim.cmd.wincmd("J")
+  vim.api.nvim_win_set_height(0, 12)
+  vim.wo.winfixheight = true
+  vim.cmd.term()
+end)
+map("n", "<M-h>", "gT")
+map("n", "<M-l>", "gt")
+
+-- yanking and putting
+map("x", "<leader>p", [["_dP]])
+map({ "n", "v" }, "<leader>y", [["+y]])
+map("n", "<leader>Y", [["+Y]])
+map({ "n", "v" }, "<leader>d", [["_d]])
+
+-- lsp format
+map("n", "<leader>f", function()
+  vim.lsp.buf.format({ timeout_ms = 2000 })
+end)
+
+map("n", "<Tab>", "<cmd>Ex<CR>")
+-- TODO
+vim.g.netrw_banner = 0
---
+++ lua/user/options.lua
@@ -0,0 +1,51 @@
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
+vim.opt.conceallevel = 2
+vim.opt.confirm = true
+vim.opt.cursorline = true
+vim.opt.cursorlineopt = "number"
+vim.opt.expandtab = true
+vim.opt.fillchars = {
+    foldopen = "",
+    foldclose = "",
+    fold = " ",
+    foldsep = " ",
+    diff = "╱",
+    eob = " ",
+}
+vim.opt.formatoptions = "jtcqn"
+-- TODO: check if the program is installed by running checkhealth
+vim.opt.grepprg = "rg --vimgrep"
+vim.opt.guicursor = "n-v-c:block,i-ci-ve:block,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250,sm:block-blinkwait175-blinkoff150-blinkon175"
+vim.opt.ignorecase = true
+vim.opt.inccommand = "split"
+vim.opt.laststatus = 3
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+vim.opt.mouse = "a"
+vim.opt.number = true
+vim.opt.pumblend = 10
+vim.opt.pumheight = 10
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 8
+vim.opt.shiftround = true
+vim.opt.shiftwidth = 4
+vim.opt.signcolumn = "yes"
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.softtabstop = 4
+vim.opt.splitbelow = true
+vim.opt.splitbelow = true
+vim.opt.splitkeep = "screen"
+vim.opt.splitright = true
+vim.opt.tabstop = 4
+vim.opt.termguicolors = true
+vim.opt.undofile = true
+vim.opt.updatetime = 200
+vim.opt.virtualedit = "block"
+vim.opt.wrap = false
+
+if vim.fn.has("nvim-0.10.0") then
+    vim.opt.smoothscroll = true
+end
+
+vim.g.mapleader = " "
