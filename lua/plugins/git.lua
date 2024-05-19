dce4a4de5004926243fe425ace612866422fed83
scimania <gauravdeepubhi@gmail.com>
2024-05-20 00:26:56 +0530

    feat: added fugitive

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -6,4 +6,12 @@ return {
             require("user.configs.git").gs.setup()
         end,
     },
+    {
+        "tpope/vim-fugitive",
+        event = { "BufReadPre", "BufNewFile" },
+        cmd = "Git",
+        keys = {
+            { "<leader><leader>g", "<cmd>Git<cr>", desc = "Fugitive" },
+        },
+    }
 }
