f154eaa0090789af1bc82b7d47f2b4079743e3f3
scimania <gauravdeepubhi@gmail.com>
2024-07-09 08:16:43 +0530

    fix: use opt_local instead of opt for spell and wrap autocmd

--- lua/user/autocmds.lua
+++ lua/user/autocmds.lua
@@ -24,7 +24,7 @@ vim.api.nvim_create_autocmd("FileType", {
     group = create_augroup("wrap_spell"),
     pattern = { "gitcommit", "markdown" },
     callback = function()
-        vim.opt.wrap = true
-        vim.opt.spell = true
+        vim.opt_local.wrap = true
+        vim.opt_local.spell = true
     end,
 })
